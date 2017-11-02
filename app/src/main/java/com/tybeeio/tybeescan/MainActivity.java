6740773084b2f7404ad59d947ad80db7fa29f37b
KChittabathini16 <ckaushik365@gmail.com>
2017-11-01 23:18:00 -0700

    Commit 6

--- app/src/main/java/com/tybeeio/tybeescan/MainActivity.java
+++ app/src/main/java/com/tybeeio/tybeescan/MainActivity.java
@@ -1,79 +1,13 @@
 package com.tybeeio.tybeescan;
 
-import android.os.Bundle;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
-import com.kontakt.sdk.android.ble.manager.ProximityManager;
-import com.kontakt.sdk.android.ble.manager.ProximityManagerFactory;
-import com.kontakt.sdk.android.ble.manager.listeners.EddystoneListener;
-import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener;
-import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleEddystoneListener;
-import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener;
-import com.kontakt.sdk.android.common.KontaktSDK;
-import com.kontakt.sdk.android.common.profile.IBeaconDevice;
-import com.kontakt.sdk.android.common.profile.IBeaconRegion;
-import com.kontakt.sdk.android.common.profile.IEddystoneDevice;
-import com.kontakt.sdk.android.common.profile.IEddystoneNamespace;
+import com.kontakt.proximity.KontaktProvider;
+import com.kontakt.proximity.KontaktProximitySDK;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ProximityManager proximityManager;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        KontaktSDK.initialize("yqXmSMOkqONhACmwQhtHhjjdPllatMrQ");
-
-        proximityManager = ProximityManagerFactory.create(this);
-        proximityManager.setIBeaconListener(createIBeaconListener());
-        proximityManager.setEddystoneListener(createEddystoneListener());
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        startScanning();     }
-
-    @Override
-    protected void onStop() {
-        proximityManager.stopScanning();
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        proximityManager.disconnect();
-        proximityManager = null;
-        super.onDestroy();
-    }
-
-    private void startScanning() {
-        proximityManager.connect(new OnServiceReadyListener() {
-            @Override
-            public void onServiceReady() {
-                proximityManager.startScanning();
-            }
-        });
-    }
-
-    private IBeaconListener createIBeaconListener() {
-        return new SimpleIBeaconListener() {
-            @Override
-            public void onIBeaconDiscovered(IBeaconDevice ibeacon, IBeaconRegion region) {
-                Log.i("Sample", "IBeacon discovered: " + ibeacon.toString());
-            }
-        };
-    }
-
-    private EddystoneListener createEddystoneListener() {
-        return new SimpleEddystoneListener() {
-            @Override
-            public void onEddystoneDiscovered(IEddystoneDevice eddystone, IEddystoneNamespace namespace) {
-                Log.i("Sample", "Eddystone discovered: " + eddystone.toString());
-            }
-        };
-    }
+    String apiKey = "yqXmSMOkqONhACmwQhtHhjjdPllatMrQ";
+    Context context = getApplicationContext();
+    KontaktProximitySDK sdk = KontaktProvider.provideProximitySDK(context, apiKey);
 }
